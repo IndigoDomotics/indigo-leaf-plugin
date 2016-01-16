91865efa37626ab77d592dadeb2408aac59812e3
Jason Horne <jason@drjason.com>
2016-01-16 15:20:13 -0500

    support multiple regions in embedded pycarwings module

--- Nissan Leaf.indigoPlugin/Contents/Server Plugin/pycarwings/connection.py
+++ Nissan Leaf.indigoPlugin/Contents/Server Plugin/pycarwings/connection.py
@@ -1,26 +1,37 @@
 import urllib2
 from xml.dom import minidom
 
+BASE_URL = {
+    "US" : "https://nissan-na-smartphone-biz.viaaq.com/aqPortal/smartphoneProxy",
+    "EU" : "https://nissan-eu-smartphone-biz.viaaq.eu/aqPortal/smartphoneProxy"
+}
+
 class Connection(object):
     """Maintains a connection to CARWINGS, refreshing it when needed"""
 
-    BASE_URL = 'https://nissan-na-smartphone-biz.viaaq.com/aqPortal/smartphoneProxy'
-
-    def __init__(self, username, password):
+    def __init__(self, username, password, region="US"):
         self.username = username
         self.password = password
         self.logged_in = False
+        self.set_region(region)
         self.connect()
 
     def connect(self):
         self.handler = urllib2.HTTPCookieProcessor()
         self.opener = urllib2.build_opener(self.handler)
 
+    def set_region(self, region):
+        self.BASE_URL = BASE_URL[region]
+        self.region = region
+
+        if self.logged_in and self.region != region:
+            self.logged_in = False
+
     def post_xml(self, service, xml_data, suppress_response=False):
         data = xml_data.toxml()
-        request = urllib2.Request("%s%s" % (self.BASE_URL, service), 
-                                  data, 
-                                  {'Content-Type': 'text/xml', 
+        request = urllib2.Request("%s%s" % (self.BASE_URL, service),
+                                  data,
+                                  {'Content-Type': 'text/xml',
                                    'User-Agent': 'NissanLEAF/1.40 CFNetwork/485.13.9 Darwin/11.0.0 pyCW'})
         response = self.opener.open(request)
         response_data = response.read()
